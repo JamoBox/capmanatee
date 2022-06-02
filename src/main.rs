5268472303f68d7e01c60fcb37ea6ad28402e6e0
Pete Wicken <petewicken@gmail.com>
2022-06-02 23:33:48 +0100

    Remove ether module which was overkill

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 // 5 tuples and number of packets for each
-mod ether;
-
 use chrono::NaiveDateTime;
 use ctrlc;
 use pcap::{Device, Packet};
@@ -9,10 +7,11 @@ use std::fmt;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 
-use ether::{to_ethertype, Ethertype};
-
 const TCP: u8 = 6;
 const UDP: u8 = 17;
+
+const IPV4: u16 = 0x0800;
+const IPV6: u16 = 0x86dd;
 
 #[derive(Eq, PartialEq, Hash)]
 struct FiveTuple {
@@ -77,6 +76,28 @@ impl fmt::Display for FiveTuple {
             l3_src, self.l4_sport, l3_dst, self.l4_dport, self.next_proto
         )
     }
+}
+
+#[repr(u16)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub enum Ethertype {
+    IPV4 = IPV4,
+    IPV6 = IPV6,
+    UNKNOWN,
+}
+
+impl fmt::Display for Ethertype {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{:?} (0x{:04X})", self, *self as u16);
+    }
+}
+
+pub fn to_ethertype(value: u16) -> Ethertype {
+    return match value {
+        IPV4 => Ethertype::IPV4,
+        IPV6 => Ethertype::IPV6,
+        _ => Ethertype::UNKNOWN,
+    };
 }
 
 fn handle_ipv4(
